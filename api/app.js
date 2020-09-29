bd672d1408c65803e5e7b01b5291e0c8267cb5b6
SimonConan <simonconan@gmail.com>
2020-09-29 17:26:57 +0200

    Get one breed done

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const criteriaRouter = require('./routes/criteria');
+const breedsRouter = require('./routes/breeds');
 
 const app = express();
 
 app
+    .use(bodyParser.json())
     .use('/criteria', criteriaRouter)
+    .use('/breeds', breedsRouter)
     .use((req, res, next) => {
         res.status(200).json({ message: 'Server UP and running' });
     });
