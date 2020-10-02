74ee06bf3eb80347d37ec25a9098b85d1875cc3c
SimonConan <simonconan@gmail.com>
2020-10-02 11:06:37 +0200

    Manage the user choice

--- frontend/src/js/_home.js
+++ frontend/src/js/_home.js
@@ -1,2 +1,9 @@
-const testDiv = document.getElementById('test');
-testDiv.textContent = 'This is a setup test 212';
+const criteriaList = document.querySelectorAll('span.criteria'),
+    remainingCriteria = document.getElementById('remaining-criteria'),
+    chosenCriteria = document.getElementById('chosen-criteria');
+
+criteriaList.forEach(criteria => {
+    criteria.addEventListener('click', () => {
+        criteria.parentElement.id === 'remaining-criteria' ? chosenCriteria.appendChild(criteria) : remainingCriteria.appendChild(criteria);
+    });
+});
